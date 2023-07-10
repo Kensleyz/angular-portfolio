9f6fd9c94a529d8b5376d686a9e2fed3fc6b634c
Kensley Naicker <kensley.naicker@euromonitor.com>
2023-07-10 09:28:26 +0200

    Friday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProfileSectionComponent } from './profile-section/profile-section.component';
+import { WorkSectionComponent } from './work-section/work-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProfileSectionComponent,
+    WorkSectionComponent
   ],
   imports: [
     BrowserModule
